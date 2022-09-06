56db4b44a6b50d51d228034de9fdfb1ddda30af0
Paul <pholladay@test.com>
2022-09-06 13:25:20 -0500

    completed employee bonus and customer country

--- print_customers.py
+++ print_customers.py
@@ -7,12 +7,12 @@ csvfile = csv.reader(infile, delimiter=',')
 next(csvfile)                                       #this will skip the first row
 
 for record in csvfile:
-    print('ID: ' + record[0])
-    print('FName: ' + record[1])
-    print('LName: ' + record[2])
-    print('City: ' + record[3])
+    print('ID: ' '\t' + record[0])
+    print('FName: ' '\t' + record[1])
+    print('LName: ' '\t' + record[2])
+    print('City: ' '\t' + record[3])
     print('Country: ' + record[4])
-    print('Phone: ' + record[5])
+    print('Phone: ' '\t' + record[5])
     print()
     input()
     
